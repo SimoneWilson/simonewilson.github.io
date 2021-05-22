0626e0896521049d628c2b98c890abaa649bc554
Simone McKee <smckee@wilsonelectronics.com>
2021-05-21 22:57:22 -0600

    bom working and css reviewed

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -3,7 +3,7 @@ document.getElementById("add-to-list").addEventListener("click", function () {
   const x = document.getElementById("favchap").value;
 
   if (x !== "") {
-    //create a new button in html
+    //create a new button
     const newBTN = document.createElement("button");
     newBTN.innerHTML = "\u274C";
 
@@ -14,9 +14,20 @@ document.getElementById("add-to-list").addEventListener("click", function () {
     document.getElementById("favs").appendChild(newLI);
     document.getElementById("favchap").value = "";
 
+    //remove the item from the list
     newBTN.addEventListener("click", function () {
       document.getElementById("favs").removeChild(newLI);
+      //set the cursor back in the input
       document.getElementById("favchap").focus();
     });
+
+    //to user the enter button
+    /* input = document.getElementById("favchap");
+    input.addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("button").click();
+      }
+    });*/
   }
 });
