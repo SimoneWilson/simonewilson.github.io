0fb5cbc25a768dd247036278e0f53f6ed7cda1ea
Simone McKee <smckee@wilsonelectronics.com>
2021-05-28 22:00:09 -0600

    lesson6 done

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -46,3 +46,27 @@ if (dayNumber == 6) {
 } else {
   element.classList.add("hideme");
 }
+
+/*----------Weather Summary----------------*/
+
+const tempString = document.getElementById("temp").textContent;
+//console.log(tempString);
+const tempNumber = parseFloat(tempString);
+//console.log(tempNumber);
+
+const speedNumber = parseFloat(document.getElementById("speed").textContent);
+//console.log(speedNumber);
+
+let windchill =
+  35.74 +
+  0.6215 * tempNumber -
+  (35.75 * Math.pow(speedNumber, 0.16) +
+    0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+windchill = Math.round(windchill);
+//console.log(windchill);
+
+if (tempNumber <= 50 && speedNumber > 3) {
+  document.getElementById("chill").textContent = windchill + "\xB0F";
+} else {
+  document.getElementById("chill").textContent = "No Wind Chill Today";
+}
