ae219641459baa5059c3314162089076c83aa7fa
Simone McKee <smckee@wilsonelectronics.com>
2021-06-04 20:38:06 -0600

    loading lazy

---
+++ lesson-7/script.js
@@ -0,0 +1,28 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+let imgOptions = {};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    }, imgOptions);
+  });
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
