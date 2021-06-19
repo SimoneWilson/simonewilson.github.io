486cbcfc1f8a1ccd49032276341e7f422693a6d0
simonewilson <smckee@wilsonelectronics.com>
2021-06-18 22:19:45 -0600

    prophets cards done

---
+++ lesson-9/prophets.js
@@ -0,0 +1,35 @@
+const requestURL =
+  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const prophets = jsonObject["prophets"];
+    for (let i = 0; i < prophets.length; i++) {
+      let card = document.createElement("section");
+      let h2 = document.createElement("h2");
+      let birthDate = document.createElement("p");
+      let birthPlace = document.createElement("p");
+      let image = document.createElement("img");
+      let currentProphet = i + 1;
+
+      h2.textContent = prophets[i].name + " " + prophets[i].lastname;
+      birthDate.textContent = prophets[i].birthdate;
+      birthPlace.textContent = prophets[i].birthplace;
+      image.setAttribute("src", prophets[i].imageurl);
+      image.setAttribute(
+        "alt",
+        prophets[i].name + " " + prophets[i].lastname + " - " + currentProphet
+      );
+
+      card.appendChild(h2);
+      card.appendChild(birthDate);
+      card.appendChild(birthPlace);
+      card.appendChild(image);
+
+      document.querySelector("div.cards").appendChild(card);
+    }
+  });
